4c68f9e9033cab43a2e7ac279335408c4ac704c1
Shadman Ahmed <shadman@outlook.com>
2020-05-13 22:21:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,13 @@
 
 def generate_star_date
-  $star_date = (rand(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(star_date)
-  puts state_log(star_date)
+def engage(date)
+  puts state_log(date)
   date = generate_star_date
 end
