d75900833aa67d0900e52bfa33a9e68c0d665222
Shadman Ahmed <shadman@outlook.com>
2020-05-13 22:22:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,7 +7,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(date)
-  puts state_log(date)
+def engage
   date = generate_star_date
+  puts state_log(date)
 end
